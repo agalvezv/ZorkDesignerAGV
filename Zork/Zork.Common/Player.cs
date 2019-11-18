f8f321bc658fd62877d145ec69b99e3bd2ddda32
agalvezv <43121059+agalvezv@users.noreply.github.com>
2019-11-18 14:18:51 -0500

    Initial commit

---
+++ Zork/Zork.Common/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Zork.Common
+{
+    public class Player
+    {
+        private int mscore;
+
+        public event EventHandler<int> ScoreChanged;
+        public World World { get; }
+
+        [JsonIgnore]
+
+        public Room Location { get; private set; }
+
+
+        public int Moves { get; set; }
+
+        public int Score
+        {
+            get => mscore;
+            set
+            {
+                mscore = value;
+                ScoreChanged?.Invoke(this, mscore);
+
+
+            }
+        }
+
+
+        [JsonIgnore]
+
+        public string LocationName
+        {
+            get
+            {
+                return Location?.Name;
+            }
+            set
+            {
+                Location = World?.RoomsByName.GetValueOrDefault(value);
+            }
+        }
+
+        public Player(World world, string startingLocation)
+        {
+            World = world;
+            LocationName = startingLocation;
+        }
+
+        public bool Move(Directions direction)
+        {
+            bool isValidMove = Location.Neighbors.TryGetValue(direction, out Room destination);
+            if (isValidMove)
+            {
+                Location = destination;
+            }
+
+            return isValidMove;
+        }
+
+    }
+}
